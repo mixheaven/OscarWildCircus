d99ea38da709f586749e961e61b724f8ccc316c4
mixheaven <mixheaven@hotmail.com>
2020-02-19 11:57:54 +0100

    first commit

---
+++ oscarwildcircus/src/test/java/com/oscarwildcircus/oscarwildcircus/OscarwildcircusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarwildcircus.oscarwildcircus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarwildcircusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
